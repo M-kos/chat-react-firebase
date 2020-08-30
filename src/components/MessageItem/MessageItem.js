beda0666719a81355d5b22131b1ba6f222a70a6d
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-30 21:20:46 +0300

    Добавил пропсы

--- src/components/MessageItem/MessageItem.js
+++ src/components/MessageItem/MessageItem.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { getDateAndTime } from '../../utils/getDateAndTime'
 
 import './MessageItem.scss'
 
-export const MessageItem = ({ message }) => {
-  const messageClassName = `messageItem ${message.self ? 'self' : ''}`
+export const MessageItem = ({ message, uId }) => {
+  const messageClassName = `messageItem ${message.uId === uId ? 'self' : ''}`
 
   return (
     <div className={messageClassName}>
       <div className="messageItem-head">
-        <div className="messageItem-head-title">{message.title}</div>
-        <div className="messageItem-head-time">{message.time}</div>
+        <div className="messageItem-head-title">{message.uId}</div>
+        <div className="messageItem-head-time">
+          {getDateAndTime(message.timestamp)}
+        </div>
       </div>
       <div className="messageItem-message">{message.text}</div>
     </div>
--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -4,9 +4,9 @@ import { MessageItem } from '../MessageItem/MessageItem'
 
 import './MesssageList.scss'
 
-export const MesssageList = ({ messsageList = [] }) => {
+export const MesssageList = ({ messsageList = [], uId }) => {
   const mapMessageList = messsageList.map((message) => {
-    return <MessageItem key={message.id} message={message} />
+    return <MessageItem key={message.id} message={message} uId={uId} />
   })
 
   return <div className="messsageList">{mapMessageList}</div>
