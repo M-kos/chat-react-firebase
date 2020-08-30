5d7bb96449cdbd9a864b1be0bda4e95b356d1ad8
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-08-30 22:02:49 +0300

    Автоматическая прокрутка вниз

--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import { MessageItem } from '../MessageItem/MessageItem'
 
 import './MesssageList.scss'
 
 export const MesssageList = ({ messsageList = [], uId }) => {
+  const messageContainer = useRef(null)
+
+  useEffect(() => {
+    messageContainer.current.scrollTop = messageContainer.current.scrollHeight
+  }, [messsageList])
+
   const mapMessageList = messsageList.map((message) => {
     return <MessageItem key={message.id} message={message} uId={uId} />
   })
 
-  return <div className="messsageList">{mapMessageList}</div>
+  return (
+    <div ref={messageContainer} className="messsageList">
+      {mapMessageList}
+    </div>
+  )
 }
